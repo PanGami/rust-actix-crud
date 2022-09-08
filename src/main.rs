5067a2b18940fc9e0d31e01fc96ac30c87a821e8
PanGami <pancanugrahawicaksana@gmail.com>
2022-09-08 07:57:05 +0700

    apply .env on port and url app

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate diesel;
 use actix_web::{middleware, web, App, HttpServer};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
+use std::env;
 
 // We define a custom type for connection pool to use later.
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
@@ -20,7 +21,10 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     // set up database connection pool
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL Must be set");
+    let app_url = env::var("APP_URL").expect("APP_URL Must be set");
+    let app_port = env::var("APP_PORT").expect("APP_PORT Must be set");
+    
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     let pool: DbPool = r2d2::Pool::builder()
         .build(manager)
@@ -37,7 +41,7 @@ async fn main() -> std::io::Result<()> {
             .service(handlers::update)
             .service(handlers::destroy)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(format!("{}:{}", app_url, app_port))?
     .run()
     .await
 }
