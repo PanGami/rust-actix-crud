63db7ed33fb55cce07db99dda058fcf797f6bf4c
PanGami <pancanugrahawicaksana@gmail.com>
2022-09-11 20:06:04 +0700

    update routes config

--- src/config/app.rs
+++ src/config/app.rs
@@ -12,19 +12,19 @@ pub fn config_path(config: &mut web::ServiceConfig) {
             .route("/", web::get().to(index)) //default endpoint
             .service(
                 web::scope("api")
-                .route("/", web::get().to(index)) //default api endpoint                       
+                .route("", web::get().to(index)) //default api endpoint                       
                 .service(
                     web::scope("tweets") // http://localhost:8080/api/tweets/
-                        .route("/", web::get().to(tweets::index))     
-                        .route("/", web::post().to(tweets::create))
+                        .route("", web::get().to(tweets::index))     
+                        .route("", web::post().to(tweets::create))
                         .route("/{id}", web::get().to(tweets::show))
                         .route("/{id}", web::delete().to(tweets::destroy))
                         .route("/{id}", web::patch().to(tweets::update))       
                 )
                 .service( //Another Exaple Endpoints
                     web::scope("test") //http://localhost:8080/api/test/
-                        .route("/", web::get().to(index))     
-                        // .route("/", web::post().to(tweets::create))
+                        .route("", web::get().to(index))     
+                        // .route("", web::post().to(tweets::create))
                         // .route("/{id}", web::get().to(tweets::show))
                         // .route("/{id}", web::delete().to(tweets::destroy))
                         // .route("/{id}", web::patch().to(tweets::update))     
