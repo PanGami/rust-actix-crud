f54f335bf4497571c2d605fe15de7c53b149e2cd
PanGami <pancanugrahawiwcaksananansbw@gmail.com>
2022-10-17 21:57:03 +0700

    fix

--- src/google/routes.rs
+++ src/google/routes.rs
@@ -2,47 +2,51 @@ extern crate google_signin;
 
 use actix_session::Session;
 use actix_web::{web, Error, HttpRequest, HttpResponse};
+use serde::{Deserialize, Serialize};
 use std::env;
 use super::action::{add_token};
 
-struct GoogleLogin {
+#[derive(Debug,Deserialize,Serialize)]
+pub struct GoogleLogin {
   token: String,
 }
+// pub async fn login(_req: HttpRequest) {
+//     dotenv::dotenv().ok();
 
-pub async fn login(_req: HttpRequest) {
-    dotenv::dotenv().ok();
-
-    let audiences = env::var("CLIENT_ID").expect("CLIENT ID Must be set");
-    let domain = env::var("HOSTED_DOMAIN").expect("HOSTED DOMAIN Must be set");
-    let mut client = google_signin::Client::new();
-    client.audiences.push(audiences);
-    client.hosted_domains.push(domain);
-}
-pub async fn create(
+//     let audiences = env::var("CLIENT_ID").expect("CLIENT ID Must be set");
+//     let domain = env::var("HOSTED_DOMAIN").expect("HOSTED DOMAIN Must be set");
+//     let mut client = google_signin::Client::new();
+//     client.audiences.push(audiences);
+//     client.hosted_domains.push(domain);
+// }
+pub async fn login(
   payload: web::Json<GoogleLogin>,
   session: Session
 ) -> Result<HttpResponse, Error> {
   dotenv::dotenv().ok();
   let audiences = env::var("CLIENT_ID").expect("CLIENT ID Must be set");
   let mut client = google_signin::Client::new();
-  client.audiences.push(audiences);
+  client.audiences.push(audiences.to_string());
 
-  let token = web::block(move || {
-    add_token(&payload.token, session);
+  add_token(&payload.token, session.clone());
 
-    let token = session.get::<&str>("token");
+  let token = session.get::<String>("token")?.unwrap();
 
-    let id_info = client.verify(&payload.token).expect("Expected token to be valid");
-    println!("Success! Signed-in as {}", id_info.sub);
-  })
-  .await?;
+  let id_info = client.verify(&payload.token).expect("Expected token to be valid");
+  println!("Success! Signed-in as {}", id_info.sub);
 
-  Ok(HttpResponse::Ok().json(token))
+  // let token = web::block(move || {
+
+  // })
+  // .await?;
+
+  Ok(HttpResponse::Ok().json("test"))
 }
-pub async fn test(session: Session) -> Result<HttpResponse, Error> {
-    if let Some(info) = session.get::<String>("info")? {
-      Ok(HttpResponse::Ok().body(format!("Here is your Session! {info}")))
-  } else {
-      Ok(HttpResponse::Ok().json("No Session data Found!"))
-    }
-  }
+
+// pub async fn test(session: Session) -> Result<HttpResponse, Error> {
+//     if let Some(info) = session.get::<String>("info")? {
+//       Ok(HttpResponse::Ok().body(format!("Here is your Session! {info}")))
+//   } else {
+//       Ok(HttpResponse::Ok().json("No Session data Found!"))
+//     }
+//   }
