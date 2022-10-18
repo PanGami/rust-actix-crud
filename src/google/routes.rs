45f09426b96229777290d1620fa2e888bfff75f7
PanGami <pancanugrahawiwcaksananansbw@gmail.com>
2022-10-18 12:16:25 +0700

    tidy up

--- src/google/routes.rs
+++ src/google/routes.rs
@@ -1,52 +1,43 @@
 extern crate google_signin;
 
 use actix_session::Session;
-use actix_web::{web, Error, HttpRequest, HttpResponse};
+use actix_web::{web, Error, HttpResponse};
 use serde::{Deserialize, Serialize};
 use std::env;
-use super::action::{add_token};
+// use super::action::{add_token};
 
 #[derive(Debug,Deserialize,Serialize)]
 pub struct GoogleLogin {
   token: String,
 }
-// pub async fn login(_req: HttpRequest) {
-//     dotenv::dotenv().ok();
 
-//     let audiences = env::var("CLIENT_ID").expect("CLIENT ID Must be set");
-//     let domain = env::var("HOSTED_DOMAIN").expect("HOSTED DOMAIN Must be set");
-//     let mut client = google_signin::Client::new();
-//     client.audiences.push(audiences);
-//     client.hosted_domains.push(domain);
-// }
 pub async fn login(
   payload: web::Json<GoogleLogin>,
   session: Session
 ) -> Result<HttpResponse, Error> {
   dotenv::dotenv().ok();
   let audiences = env::var("CLIENT_ID").expect("CLIENT ID Must be set");
+  let domain = env::var("HOSTED_DOMAIN").expect("HOSTED DOMAIN Must be set");
   let mut client = google_signin::Client::new();
-  client.audiences.push(audiences.to_string());
 
-  add_token(&payload.token, session.clone());
+  client.audiences.push(audiences.to_string());
+  client.hosted_domains.push(domain.to_string());
+
+  session.insert("token", &payload.token)?;
 
   let token = session.get::<String>("token")?.unwrap();
 
-  let id_info = client.verify(&payload.token).expect("Expected token to be valid");
-  println!("Success! Signed-in as {}", id_info.sub);
+  // recommended
+  let id_info = client.verify(&token).expect("Expected token to be valid");
+  // println!("Success! Signed-in as {}", id_info.sub);
 
-  // let token = web::block(move || {
+  // Inspect the ID before verifying it
+  //let id_info = client.get_slow_unverified(&token).expect("Expected token to exist");
+  // let ok = id_info.verify(&client).is_ok();
+  // println!("Ok: {}, Info: {:?}", ok, id_info);
 
-  // })
-  // .await?;
+  // let id_info = "test";
 
-  Ok(HttpResponse::Ok().json("test"))
+  Ok(HttpResponse::Ok().json(format!("token : {}, info : {:?}", &token, &id_info)))
 }
-
-// pub async fn test(session: Session) -> Result<HttpResponse, Error> {
-//     if let Some(info) = session.get::<String>("info")? {
-//       Ok(HttpResponse::Ok().body(format!("Here is your Session! {info}")))
-//   } else {
-//       Ok(HttpResponse::Ok().json("No Session data Found!"))
-//     }
-//   }
+//rust jwk convert
