76785f0c08ccf920c1aab859bbda8ae85a422383
PanGami <pancanugrahawiwcaksananansbw@gmail.com>
2022-10-18 12:49:04 +0700

    tidy up

--- src/google/action.rs
+++ src/google/action.rs
@@ -2,7 +2,7 @@ use actix_session::Session;
 use actix_web::{Error, HttpResponse};
 
 // Functions define
-pub fn add_token(_token: &str, session: Session) -> Result<HttpResponse, Error>{
-    session.insert("token", _token);
-    Ok(HttpResponse::Ok().json("Session value Added"))
-}
+// pub fn add_token(_token: &str, session: Session) -> Result<HttpResponse, Error>{
+//     session.insert("token", _token);
+//     Ok(HttpResponse::Ok().json("Session value Added"))
+// }
